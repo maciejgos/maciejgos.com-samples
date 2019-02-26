2901f8f0b8c92fa0b8bbc508b1cfabfac6db8ac6
Maciej Gos <maciej@maciejgos.com>
2019-02-26 19:40:51 +0100

    Refactor

--- azure-table-storage/table-patterns/Program.cs
+++ azure-table-storage/table-patterns/Program.cs
@@ -11,26 +11,26 @@ namespace AzureTableStorage.Patterns
 {
     class Program
     {
-        static string connectionString;
+        static string _connectionString;
+        static CloudStorageAccount _storageAccount;
+        static CloudTableClient _tableClient;
+        static CloudTable _table;
 
         static void Main(string[] args)
         {
             ReadConfiguration();
+            Initialize();
 
             // Index entities pattern
-            System.Console.WriteLine("Index entities pattern");
+            Console.WriteLine("Index entities pattern");
             CreateIndexEntitiesPatternDemo().Wait();
         }
 
         static async Task CreateIndexEntitiesPatternDemo()
         {
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
-            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
-            CloudTable table = tableClient.GetTableReference("movies");
-
             // Create table if not exists
             Console.WriteLine("...Create table if not exists...");
-            await table.CreateIfNotExistsAsync();
+            await _table.CreateIfNotExistsAsync();
 
             // Collection of ids
             List<string> moviesType1 = new List<string>();
@@ -90,12 +90,12 @@ namespace AzureTableStorage.Patterns
 
                 batchOperation3.Insert(movie);
             }
-            
+
             // Execute batch
             Console.WriteLine("...Execute batch operations...");
-            await table.ExecuteBatchAsync(batchOperation1);
-            await table.ExecuteBatchAsync(batchOperation2);
-            await table.ExecuteBatchAsync(batchOperation3);
+            await _table.ExecuteBatchAsync(batchOperation1);
+            await _table.ExecuteBatchAsync(batchOperation2);
+            await _table.ExecuteBatchAsync(batchOperation3);
 
             // Insert Index Entities
             var index1 = new Index(partitionKey: "Comedy", rowKey: "2011") { IndexEntities = string.Join(',', moviesType1.ToArray()) };
@@ -106,23 +106,23 @@ namespace AzureTableStorage.Patterns
 
             // Execute insert operations
             Console.WriteLine("...Execute insert operations...");
-            await table.ExecuteAsync(insert1);
-            await table.ExecuteAsync(insert2);
-            
+            await _table.ExecuteAsync(insert1);
+            await _table.ExecuteAsync(insert2);
+
 
             // Retrive index
             Console.WriteLine("...Retrive index...");
             TableQuery<Index> rangeQuery1 = new TableQuery<Index>()
                 .Where(TableQuery.CombineFilters(
                     TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "Comedy")
-                    ,TableOperators.And
-                    ,TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, "2011")));
+                    , TableOperators.And
+                    , TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, "2011")));
 
-            var result1 = await table.ExecuteQuerySegmentedAsync(rangeQuery1, null);
-            string[] movieTitles = {};
+            var result1 = await _table.ExecuteQuerySegmentedAsync(rangeQuery1, null);
+            string[] movieTitles = { };
             foreach (var index in result1)
             {
-                Console.WriteLine($"{nameof(index.PartitionKey)}: {index.PartitionKey}, {nameof(index.RowKey)}: {index.RowKey}, {nameof(index.IndexEntities)}: {index.IndexEntities}" );
+                Console.WriteLine($"{nameof(index.PartitionKey)}: {index.PartitionKey}, {nameof(index.RowKey)}: {index.RowKey}, {nameof(index.IndexEntities)}: {index.IndexEntities}");
                 movieTitles = index.IndexEntities.Split(',');
             }
 
@@ -138,7 +138,7 @@ namespace AzureTableStorage.Patterns
                 TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.LessThanOrEqual, "Movie 10")))
             );
 
-            var movies = await table.ExecuteQuerySegmentedAsync(query, null);
+            var movies = await _table.ExecuteQuerySegmentedAsync(query, null);
             foreach (var movie in movies)
             {
                 Console.WriteLine($"Movie Type: {movie.Type}, Premiere: {movie.Premiere}, Title: {movie.Title}");
@@ -146,7 +146,14 @@ namespace AzureTableStorage.Patterns
 
             // Remove data / Cleanup
             Console.WriteLine("...Cleanup...");
-            await table.DeleteIfExistsAsync();
+            await _table.DeleteIfExistsAsync();
+        }
+
+        private static void Initialize()
+        {
+            _storageAccount = CloudStorageAccount.Parse(_connectionString);
+            _tableClient = _storageAccount.CreateCloudTableClient();
+            _table = _tableClient.GetTableReference("movies");
         }
 
         static void ReadConfiguration()
@@ -155,9 +162,9 @@ namespace AzureTableStorage.Patterns
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .Build();
 
-            connectionString = config["connectionString"];
+            _connectionString = config["connectionString"];
 
-            Debug.WriteLine($"Read param: [Connection String] - [Value]: {connectionString}");
+            Debug.WriteLine($"Read param: [Connection String] - [Value]: {_connectionString}");
         }
     }
 }
