798eededd988e569584d641fc97ec9d0b568bd53
Maciej Gos <maciej@maciejgos.com>
2019-02-27 10:37:25 +0100

    Log tail pattern

--- azure-table-storage/table-patterns/Program.cs
+++ azure-table-storage/table-patterns/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using AzureTableStorage.Patterns.Models;
 using Microsoft.Extensions.Configuration;
@@ -24,10 +25,75 @@ namespace AzureTableStorage.Patterns
             // Index entities pattern
             Console.WriteLine("Index entities pattern");
             CreateIndexEntitiesPatternDemo().Wait();
+
+            Console.WriteLine("Log tail pattern");
+            CreateLogTailPatternDemo().Wait();
+        }
+
+        private static async Task CreateLogTailPatternDemo()
+        {
+            _table = _tableClient.GetTableReference("orders");
+
+            // Create table if not exists
+            Console.WriteLine("...Create table if not exists...");
+            await _table.CreateIfNotExistsAsync();
+
+            string invertedTicks = string.Empty;
+            TableBatchOperation insertOperation = new TableBatchOperation();
+            // User add movie to cart.
+            invertedTicks = string.Format("{0:D19}", DateTime.MaxValue.Ticks - DateTime.UtcNow.Ticks);
+            var matrixAddToCart = new Order { PartitionKey = "CLIENT_test@test.pl", RowKey = invertedTicks,  Event = "ADD_TO_CART", Title = "The Matrix" };
+            insertOperation.Insert(matrixAddToCart);
+
+            // User change his mind and remove movie from cart.
+            invertedTicks = string.Format("{0:D19}", DateTime.MaxValue.Ticks - DateTime.UtcNow.Ticks);
+            var removeFromCart = new Order { PartitionKey = "CLIENT_test@test.pl", RowKey = invertedTicks,  Event = "REMOVE_FROM_CART", Title = "The Matrix" };
+            insertOperation.Insert(removeFromCart);
+
+            // User add movie to cart.
+            invertedTicks = string.Format("{0:D19}", DateTime.MaxValue.Ticks - DateTime.UtcNow.Ticks);
+            var horribleBossesaddToCart = new Order { PartitionKey = "CLIENT_test@test.pl", RowKey = invertedTicks,  Event = "ADD_TO_CART", Title = "Horrible Bosses" };
+            insertOperation.Insert(horribleBossesaddToCart);
+
+            // User add provide card details.
+            invertedTicks = string.Format("{0:D19}", DateTime.MaxValue.Ticks - DateTime.UtcNow.Ticks);
+            var creditCardDetails = new Order { PartitionKey = "CLIENT_test@test.pl", RowKey = invertedTicks,  Event = "ADD_CARD_DETAILS", Title = "Horrible Bosses" };
+            insertOperation.Insert(creditCardDetails);
+
+            // User add provide card details.
+            invertedTicks = string.Format("{0:D19}", DateTime.MaxValue.Ticks - DateTime.UtcNow.Ticks);
+            var placeOrder = new Order { PartitionKey = "CLIENT_test@test.pl", RowKey = invertedTicks,  Event = "PLACE_ORDER", Title = "Horrible Bosses" };
+            insertOperation.Insert(placeOrder);
+
+            // User start watching a movie.
+            invertedTicks = string.Format("{0:D19}", DateTime.MaxValue.Ticks - DateTime.UtcNow.Ticks);
+            var startWatching = new Order { PartitionKey = "CLIENT_test@test.pl", RowKey = invertedTicks,  Event = "START_WATCHING", Title = "Horrible Bosses" };
+            insertOperation.Insert(startWatching);
+
+            // Execute batch operation
+            Console.WriteLine("...Execute batch operation...");
+            await _table.ExecuteBatchAsync(insertOperation);
+
+            // Retrive data
+            TableQuery<Order> query = new TableQuery<Order>();
+            query.Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "CLIENT_test@test.pl"));
+            var orders = await _table.ExecuteQuerySegmentedAsync(query, null);
+            foreach(var order in orders.OrderBy(o => o.ETag))
+            {
+                DateTime datetime = new DateTime(DateTime.MaxValue.Ticks - Int64.Parse(order.RowKey));
+                Console.WriteLine($"Order: {datetime}; Event type: {order.Event}; Item: {order.Title}");
+            }
+
+            // Remove data / Cleanup
+            Console.WriteLine("...Cleanup...");
+            await _table.DeleteIfExistsAsync();
+            _table = null;
         }
 
         static async Task CreateIndexEntitiesPatternDemo()
         {
+            _table = _tableClient.GetTableReference("movies");
+
             // Create table if not exists
             Console.WriteLine("...Create table if not exists...");
             await _table.CreateIfNotExistsAsync();
@@ -147,13 +213,13 @@ namespace AzureTableStorage.Patterns
             // Remove data / Cleanup
             Console.WriteLine("...Cleanup...");
             await _table.DeleteIfExistsAsync();
+            _table = null;
         }
 
         private static void Initialize()
         {
             _storageAccount = CloudStorageAccount.Parse(_connectionString);
             _tableClient = _storageAccount.CreateCloudTableClient();
-            _table = _tableClient.GetTableReference("movies");
         }
 
         static void ReadConfiguration()
